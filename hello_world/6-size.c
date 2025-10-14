e9e96bf055ddc849684188af949c57a1e06bdd50
MattW-12106 <12106@holbertonstudents.com>
2025-10-14 09:22:06 +0000

    data type sizes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,14 +5,11 @@
  * Return: Always 0 (Success)
  */
 
-int main() {
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of char: %zu byte\n", sizeof(char));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of long: %zu bytes\n", sizeof(long));
-    printf("Size of short: %zu bytes\n", sizeof(short));
-    printf("Size of long long: %zu bytes\n", sizeof(long long));
-    printf("Size of long double: %zu bytes\n", sizeof(long double));
-    return 0;
+int main(void)
+{
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
+(return 0);
 }
