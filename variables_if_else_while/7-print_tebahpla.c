84931ae775e7bb07a853822d126f0c8a92d1d11e
MattW-12106 <12106@holbertonstudents.com>
2025-10-20 09:10:00 +0000

    tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+
+	while (ch >= 'a')
+	{
+	putchar(ch);
+	ch--;
+	}
+
+	putchar('\n');
+
+return (0);
+}
