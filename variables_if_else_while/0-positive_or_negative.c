aa6afde5f62377913c4fde645914757a950a445e
MattW-12106 <12106@holbertonstudents.com>
2025-10-20 01:39:39 +0000

    is negative or positive?

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,18 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+gcc -o 0-positive_or_negative.c
+
 int main(void)
 { int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int randomNumber = rand();
+if (n > 0) {
+printf("%d is positive\n", number);
+} else if (n < 0) {
+printf("%d is negative\n", number);
+} else {
+printf("is zero\n");
+}
 return (0);
 }
