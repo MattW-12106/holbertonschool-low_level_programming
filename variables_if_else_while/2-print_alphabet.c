348c1435ce48505c653c651bd003a80879002673
MattW-12106 <12106@holbertonstudents.com>
2025-10-20 06:04:19 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+{
+	putchar(ch);
+	ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
