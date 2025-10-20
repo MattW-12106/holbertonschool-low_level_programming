498ff7223a2684a684d65585bbe8000bc166493d
MattW-12106 <12106@holbertonstudents.com>
2025-10-20 10:34:02 +0000

    unique combinations

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,20 +9,17 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-	for (j = i + 1; j <= 9; j++)
-	{
 	putchar(i + '0');
-	putchar(j + '0');
-	if (i != 8 || j != 9)
+	if (i != 9)
 	{
 	putchar(',');
+	putchar(' ');
 		}
 	}
-}
 putchar('\n');
 return (0);
 }
