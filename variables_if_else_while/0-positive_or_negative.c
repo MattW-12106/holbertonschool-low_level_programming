6c0ab96df66cea6b7c3c2e3ea4490991102fad69
MattW-12106 <12106@holbertonstudents.com>
2025-10-20 02:25:06 +0000

    positivity!

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0) {
-             printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n < 0) {
-                  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
-else {
-       printf("is zero\n");
+else (n = 0) {
+printf("is zero\n");
 }
 
 return (0);
