9b43cbdbdc4879063e353e7594f39e1a9bd4d2a2
MattW-12106 <12106@holbertonstudents.com>
2025-11-02 08:21:00 +0000

    im going to explode

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcat - concatentates two strings
+ *
+ * @dest: destination
+ *
+ * @src: source
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
