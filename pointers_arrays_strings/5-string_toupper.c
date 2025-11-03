de36f7fd4da58674fec111823d3a7c9686bc88ce
MattW-12106 <12106@holbertonstudents.com>
2025-11-03 04:01:08 +0000

    okay maybe we're cooking

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * string_toupper - change a string to uppercase
+ *
+ * @str: string
+ *
+ * Return: str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
