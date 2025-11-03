e94e3ebe16c8db90da4b199072665e22fabaf598
MattW-12106 <12106@holbertonstudents.com>
2025-11-03 05:05:46 +0000

    oh god

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ *
+ * @str: string
+ *
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char alpha[80] = "aAeEoOtTlL";
+	char numbers[80] = "43071";
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (str[i] == alpha[j])
+			{
+				str[i] = numbers[j / 2];
+			}
+		}
+	}
+	return (str);
+}
