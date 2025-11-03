57004955704e75e88c681a9b69b39bc085ccb0f0
MattW-12106 <12106@holbertonstudents.com>
2025-11-03 01:09:05 +0000

    please save me

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n amount of numbers from src and dest
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number of characters
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+return (dest);
+}
