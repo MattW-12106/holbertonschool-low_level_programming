e97bf708816c65ddb37132d2848f50715d97f854
MattW-12106 <12106@holbertonstudents.com>
2025-11-03 00:42:13 +0000

    aaaaaaaaaaaa

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings with a max num of chars to append (n)
+ *
+ *
+ *
+ *
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+return (dest);
+}
