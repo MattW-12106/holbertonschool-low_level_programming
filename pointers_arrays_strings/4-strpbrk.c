77fb4b3d95ce0d094d163640c2cb115274a325cd
MattW-12106 <12106@holbertonstudents.com>
2025-11-04 06:38:52 +0000

    not too bad

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes.
+ *
+ * @s: char pointer
+ *
+ * @accept: char pointer
+ *
+ * Return: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+
+	for (; *s != '\0'; s++)
+	{
+		for (i = 0; accept[i] != '\0'; i++)
+		{
+
+			if (*s == accept[i])
+				return (s);
+		}
+	}
+	return (0);
+}
