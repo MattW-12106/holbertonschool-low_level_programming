4a7dee8986cd4286d4cf691703335f13e89eebf4
MattW-12106 <12106@holbertonstudents.com>
2025-11-05 02:23:54 +0000

    recurse this bozo!

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string using a recursion
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		return;
+	}
+	_puts_recursion(s + 1);
+}
