3a40a660adc6ac50d6e4321d6df0552a414eadf7
MattW-12106 <12106@holbertonstudents.com>
2025-12-01 23:52:17 +0000

    readme + main.h + first question

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads the textfile and prints to output
+ * @filename: the file
+ * @letters: the text
+ * Return: returns bytes written
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer
+	ssize_t bytesRead, bytesWritten;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) = letters)
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytesREAD = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	free(buffer);
+	close(fd);
+
+	if (bytesWritten == -1 || bytesWritten != bytesRead)
+		return (0);
+
+	return (bytesWritten);
+}
---
+++ file_io/README.md
@@ -0,0 +1,1 @@
+read this and you shall perish
