9f31ec49d776210dbfff87f9436311155d98ed70
MattW-12106 <12106@holbertonstudents.com>
2025-12-01 02:43:31 +0000

    README + header + question 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,14 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t c = 0;
+	size_t c = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        c++;
-        h = h->next;
-    }
+	while (h != NULL)
+    	{
+        	printf("%d\n", h->n);
+        	c++;
+	 	h = h->next;
+    	}
 
-    return (c);
+	return (c);
 }
