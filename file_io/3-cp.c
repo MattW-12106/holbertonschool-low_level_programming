0fe40eeb85c8b01ad42aef8df37c5b695ff454d4
MattW-12106 <12106@holbertonstudents.com>
2025-12-02 02:36:49 +0000

    redfined stderr

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,7 +1,6 @@
 #ifndef ERR_FD
 #define ERR_FD STDERR_FILENO
 #endif
-
 #include "main.h"
 
 /**
@@ -10,68 +9,42 @@
  * @argv: arg vector
  * Return: 0 on success
  */
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char buffer[1024];
 	ssize_t bytesread, byteswrite;
 	int fdfrom, fdto;
 
 	if (argc != 3)
-	{
-		dprintf(ERR_FD, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+	{	dprintf(ERR_FD, "Usage: cp file_from file_to\n");
+		exit(97); }
 	fdfrom = open(argv[1], O_RDONLY);
-
 	if (fdfrom == -1)
-	{
-		dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
+	{	dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	fdto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fdto == -1)
-	{
-		dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
+	{	dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
 	while ((bytesread = read(fdfrom, buffer, 1024)) > 0)
-	{
-		byteswrite = write(fdto, buffer, bytesread);
-
+	{	byteswrite = write(fdto, buffer, bytesread);
 		if (byteswrite == -1)
-		{
-			dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
+		{	dprintf(ERR_FD, "Error: Can't write to %s\n", argv[2]);
 			close(fdto);
 			close(fdfrom);
-			exit(99);
-		}
+			exit(99); }
 	}
-
 	if (bytesread == -1)
-	{
-		dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
+	{	dprintf(ERR_FD, "Error: Can't read from file %s\n", argv[1]);
 		close(fdto);
 		close(fdfrom);
-		exit(98);
-	}
-
+		exit(98); }
 	if (close(fdto) == -1)
-	{
-		dprintf(ERR_FD, "Error: Can't close fd %d\n", fdto);
-		exit(100);
-	}
-
+	{	dprintf(ERR_FD, "Error: Can't close fd %d\n", fdto);
+		exit(100); }
 	if (close(fdfrom) == -1)
-	{
-		dprintf(ERR_FD, "Error: Can't close fd %d\n", fdfrom);
-		exit(100);
-	}
-
+	{	dprintf(ERR_FD, "Error: Can't close fd %d\n", fdfrom);
+		exit(100); }
 	return (0);
 }
 
