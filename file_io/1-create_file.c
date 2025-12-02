e62f482a9a042fc6036ce5fb7754c13662d97804
MattW-12106 <12106@holbertonstudents.com>
2025-12-02 00:28:01 +0000

    the question

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		while (text_content[len] != '\0')
 			len++;
-		i = write (fd, text_content, len);
+		i = write(fd, text_content, len);
 
 		if (i == -1)
 		{
